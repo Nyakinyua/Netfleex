143f29d6d1eaef1ba23063f096ae180808664ea5
Nyakinyua <wanyakinyua968@gmail.com>
2022-02-03 19:32:50 +0300

    Create Models to form the Movie,customer and Profile Objects

--- netfleex/models.py
+++ netfleex/models.py
@@ -1,3 +1,25 @@
+from pyexpat import model
+from turtle import title
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
+AGE_CHOICES(
+    ('All','All')
+    ('Kids','Kids')
+)
 # Create your models here.
+class CustomUser(AbstractUser):
+    profiles=models.ManyToManyField('Profile',null=True,blank=True)
+
+class Profile(models.Model):
+    name=models.CharField(max_length=225)
+    age_limit=models.CharField(max_length=10,choices=AGE_CHOICES)
+    uuid=models.UUIDField(default=uuid.uuid4)
+
+class Movie(models.Model):
+    title=models.CharField(max_length=225)
+    description=models.TextField(blank=True,null=True)
+    created=models.DateTimeField(auto_now_add=True)
+    
